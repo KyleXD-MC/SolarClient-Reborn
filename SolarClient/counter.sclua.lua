2d398fb4968a557524ead3a043c008a28db0e896
KyleXD MC <165631517+KyleXD-MC@users.noreply.github.com>
2024-04-09 16:28:40 +0500

    Create counter.sclua.lua

---
+++ SolarClient/counter.sclua.lua
@@ -0,0 +1,5 @@
+-- FileType = "Lua 5.1+"
+-- FileFormat = "sclua"
+-- sclua = solarcleintlua
+local numbers = {"1","2","3",","4","5","6","7","8","9"}
+  return(function()local a =--[[Game]]{};local J={ETGXbemEhXeyxeGDXBtexeg};gEXfvfxVEtXENHXwcvhnuexneXFgbhENXencgbEGYXbedxEX(E)CenxgeXBxEGdXEGXbebhecnegncExbencegxBEnhCFRGBEXbbmeCBMGBmbyggbyr,cMRCFBgybmgyrcbgmcRBGMgyrfcmyxebgcgby,BEDXTntuEDVfcvtudvtwfcmnWCFMncfmw}(..decall..)
